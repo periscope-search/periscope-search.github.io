a06527ffd04e1c490469e9e5fb360245d299ee3b
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-04-20 19:02:21 +1200

    Basic UI

---
+++ search.js
@@ -0,0 +1,23 @@
+const user = new URLSearchParams(location.search).get("user");
+
+fetch(`https://periscope-api.vercel.app/api/users/${user}`).then((res) => {
+  if(!res.ok) {
+    // there's a problem, error handling soon
+  }
+  return res.json()
+}).then((data) => {
+  // data received
+  document.getElementById("pfp").src = data.profile.images['90x90'];
+  document.getElementById("username").innerText = data.username;
+  document.getElementById("user-id").innerText = "#" + data.id;
+
+  document.getElementById("country").innerText = data.profile.country;
+  document.getElementById("join-date").innerText = Math.floor((new Date().getTime() - new Date(data.history.joined).getTime()) / (1000 * 60 * 60 * 24)) + " days ago"
+  document.getElementById("messages").innerText = data.messages;
+
+  document.getElementById("about-me").innerText = data.profile.bio;
+  document.getElementById("wiwo").innerText = data.profile.status;
+
+  document.getElementById("info").style.display = "inline";
+  document.getElementById("loading").style.display = "none";
+})
